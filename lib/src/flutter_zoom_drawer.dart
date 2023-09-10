d309673fce7be1b3234e3cbb118e921359ead36c
Elijah Adeniyi <dammyelie@gmail.com>
2023-09-10 02:54:30 +0100

    debug

--- lib/src/flutter_zoom_drawer.dart
+++ lib/src/flutter_zoom_drawer.dart
@@ -482,7 +482,7 @@ class ZoomDrawerState extends State<ZoomDrawer> with SingleTickerProviderStateMi
     final rotationAngle = ((((angle ?? widget.angle) * pi) / 180) * _animationValue) * _slideDirection;
 
     return Transform(
-      transform: Matrix4.translationValues(-xPosition, 0.0, 0.0)
+      transform: Matrix4.translationValues(xPosition, 0.0, 0.0)
         ..rotateZ(-rotationAngle)
         ..scale(scalePercentage, scalePercentage),
       alignment: widget.isRtl ? Alignment.centerRight : Alignment.centerLeft,
